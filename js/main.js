074b77548bd622a5617d77778bcb4eb2908eb032
Mishyn Aliaksei <trouvere777@mail.ru>
2020-05-06 23:35:55 +0300

    Day 1 Added work with login

--- js/main.js
+++ js/main.js
@@ -8,3 +8,78 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+//Day 1
+
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+
+let login = localStorage.getItem("delivery-food");
+
+function toogleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+function autorized() {
+  console.log("autorized");
+
+  function logOut(event) {
+    event.preventDefault();
+    login = null;
+    localStorage.removeItem("delivery-food");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
+    checkAuth();
+  }
+
+  userName.textContent = login;
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+
+  buttonOut.addEventListener("click", logOut);
+}
+
+function nonAautorized() {
+  console.log("nonAautorized");
+
+  function logIn(event) {
+    event.preventDefault();
+    console.log("logIn");
+    login = loginInput.value;
+
+    if (!login) {
+      alert("Требуется ввести логин");
+    } else {
+      localStorage.setItem("delivery-food", login);
+      logInForm.reset();
+      toogleModalAuth();
+    }
+    buttonAuth.removeEventListener("click", toogleModalAuth);
+    closeAuth.removeEventListener("click", toogleModalAuth);
+    logInForm.removeEventListener("submit", logIn);
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener("click", toogleModalAuth);
+  closeAuth.addEventListener("click", toogleModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    autorized();
+  } else {
+    nonAautorized();
+  }
+}
+
+checkAuth();
